c7b587cacd8ccb703b22e65e1eb4347eae004828
Bean Robinson <bentr99@gmail.com>
2022-07-23 21:11:59 +0100

    update to art.js - removed my file names

--- art.js
+++ art.js
@@ -1,18 +1,13 @@
 //This should be a list of the files in ./img
 let images = [
-	"cat.gif",
-	"faust.png",
-	"faust-office.png",
-	"taric.png",
-	"sin.png",
-	"faust-scalpel.png",
-	"faust-blue.png",
-	"plague.png",
 ];
 
+//Uncomment to pick random image from the list
 num = images.length;
 image = images[Math.floor(Math.random() * num)];
-// image = "plague.png"
+
+//Uncomment to pick specific image
+// image = images[0];
 
 let imageHTML = document.getElementById("image");
 imageHTML.innerHTML = "<img src='./art/" + image + "'/>";
